a2c418ec4e799aaa0e4272f21cf000aa0a50e2d3
Hoang Nguyen <hoang_hoang5309@hotmail.com>
2020-11-09 13:18:26 -0500

    Bug Fix

--- webScrapping/webScrapping.py
+++ webScrapping/webScrapping.py
@@ -9,7 +9,7 @@ root= tk.Tk()
 canvas1 = tk.Canvas(root, width = 300, height = 300)
 canvas1.pack()
 
-def hello ():  
+def scraping ():  
     URL = 'https://www.truepeoplesearch.com/results?name=Hoang%20Nguyen&citystatezip=Spring%20Valley,%20NY'
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -31,7 +31,7 @@ def hello ():
     label1 = tk.Label(root, text= 'Hello World!', fg='green', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 200, window=label1)
     
-button1 = tk.Button(text='Click Me',command=hello, bg='brown',fg='white')
+button1 = tk.Button(text='Click Me',command=scraping, bg='brown',fg='white')
 canvas1.create_window(150, 150, window=button1)
 
 root.mainloop()
