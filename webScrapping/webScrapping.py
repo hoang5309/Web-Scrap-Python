b84ea077d6f22c41a62c40f4d652e76d62daae5e
Hoang Nguyen <hoang_hoang5309@hotmail.com>
2021-01-20 23:06:06 -0500

    https://safer.fmcsa.dot.gov/
    Work for https://safer.fmcsa.dot.gov/

--- webScrapping/webScrapping.py
+++ webScrapping/webScrapping.py
@@ -10,9 +10,14 @@ from csv import reader
 
 root= tk.Tk()
 
-canvas1 = tk.Canvas(root, width = 400, height = 400)
+canvas1 = tk.Canvas(root, width = 500, height = 500)
 canvas1.pack()
-    
+
+global crassociationPath
+global crassociationFile
+global crassociationWriter
+   
+# Scrapping True People Search
 def selectFile ():
     filename = filedialog.askopenfilename()
     filePath = os.path.dirname(filename)
@@ -36,7 +41,7 @@ def selectFile ():
             writer.writerow(data)
     label1 = tk.Label(root, text= 'DONE!', fg='green', font=('helvetica', 12, 'bold'))
     canvas1.create_window(150, 100, window=label1)
-# Scrapping True People Search
+
 def scraping (URL, rowData):  
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -173,28 +178,37 @@ def scrapingNPI():
 
 # Scrapping Crassociation
 def loopThroughAlphabet():
+    crassociationPath = filedialog.askdirectory()
+    crassociationFile = crassociationPath+'/'+'crassociation-2.csv'
+    with open(crassociationFile, 'w', newline='') as csvfile:
+        fieldnames = ['Business Name', 'Address', 'City', 'State', 'ZIP', 'Phone', 'Fax', 'Email', 'Website', 'Description', 'Services Offered', 'Other Services Provided', 'Service Areas', 'Current Certifications']
+        crassociationWriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        crassociationWriter.writeheader()
+
+        headers = requests.utils.default_headers()
+        headers.update({
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+        })
+        alphabet = ['a','b','c','d','e','f','g',
+                    'h','i','j','k','l','m','n',
+                    'o','p','q','r','s','t','u',
+                    'v','w','x','y','z','0-9']
+        alphabetTest = ['t','u',
+                    'v','w','x','y','z','0-9']
+        pageNumber = 1
+        for x in alphabetTest:
+            time.sleep(2)
+            loopThroughPaginator('https://www.crassociation.org/consumer-services/members/list/alpha/'+x, crassociationWriter)
+
+        label1 = tk.Label(root, text= 'DONE!', fg='green', font=('helvetica', 12, 'bold'))
+        canvas1.create_window(200, 300, window=label1)
+
+def loopThroughPaginator(link, writer):
     headers = requests.utils.default_headers()
     headers.update({
         'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
     })
-    alphabet = ['a','b','c','d','e','f','g',
-                'h','i','j','k','l','m','n',
-                'o','p','q','r','s','t','u',
-                'v','w','x','y','z','0-9']
-    pageNumber = 1
-    for x in alphabet:
-        time.sleep(2)
-        loopThroughPaginator('https://www.crassociation.org/consumer-services/members/list/alpha/'+x)
-
-    label1 = tk.Label(root, text= 'DONE!', fg='green', font=('helvetica', 12, 'bold'))
-    canvas1.create_window(200, 300, window=label1)
-
-def loopThroughPaginator(link):
-    headers = requests.utils.default_headers()
-    headers.update({
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
-    })
-    getData(link)
+    getData(link, writer)
     page2 = requests.get(link, headers=headers)
     soup2 = BeautifulSoup(page2.content,"lxml")
     for ul in soup2.find_all('div', class_='pagination-centered'):
@@ -205,9 +219,9 @@ def loopThroughPaginator(link):
                     print("End of Paginator")
             else:
                 nextLink = nextBtn.findAll("a")[0]['href']
-                getData('https://www.crassociation.org/'+nextLink)
+                getData('https://www.crassociation.org/'+nextLink, writer)
 
-def getData(link):
+def getData(link, writer):
         print(link)
         headers = requests.utils.default_headers()
         headers.update({
@@ -217,19 +231,188 @@ def getData(link):
         soup3 = BeautifulSoup(page3.content,"lxml")
         for h2 in soup3.find_all('h2', class_="page-header"):
             time.sleep(2)
+            website = ""
             dataLink = h2.find_all('a')[0]['href']
-            page4 = requests.get('https://www.crassociation.org/'+link, headers=headers)
+            print('https://www.crassociation.org/'+dataLink)
+            page4 = requests.get('https://www.crassociation.org/'+dataLink, headers=headers)
             soup4 = BeautifulSoup(page4.content,"lxml")
             for SPdetail in soup4.find_all("div",  class_='SPDetailEntry'):
-                print("12/17/2020 - 12 54")
+                try:
+                    name = SPdetail.find_all("h1")[0].text
+                    address = SPdetail.find_all("div")[1].text
+                    city = SPdetail.find_all("div")[2].text
+                    state = SPdetail.find_all("div")[3].text
+                    zip = SPdetail.find_all("div")[4].text
+                    phone = SPdetail.find_all("div")[5].text
+                    fax = SPdetail.find_all("div")[6].text
+                    email = SPdetail.find_all("div")[7].find_all("a")[0].text
+                    try:
+                        website = SPdetail.find_all("div")[8].find_all("a")[0].text
+                    except:
+                        print("Error website")
+                    desc = ""
+                    for p in SPdetail.find_all("div")[9]:
+                        try:
+                            desc += p.text
+                        except:
+                            print("Error Description")
+                    services = ""
+                    for ul in SPdetail.find_all("div")[10].find_all("ul"):
+                        for li in ul.find_all("li"):
+                            try:
+                                services += li.text + " - "
+                            except:
+                                print("Error services")
+                    otherservices = SPdetail.find_all("div")[11].text
+                    area = SPdetail.find_all("div")[12].text
+                    certificate = "";
+                    for ul in SPdetail.find_all("div")[13].find_all("ul"):
+                        for li in ul.find_all("li"):
+                            try:
+                                certificate += li.text + " - "
+                            except:
+                                print("Error certificate")
+                except:
+                    print("Everything wrong")
+                writer.writerow(
+                    {'Business Name': name,
+                     'Address': address, 'City': city, 'State': state, 'ZIP': zip,
+                     'Phone': phone, 'Fax': fax, 'Email': email, 'Website': website,
+                     'Description': desc, 'Services Offered': services, 'Other Services Provided':otherservices, 'Service Areas':area, 'Current Certifications': certificate})
 
+# Scrapping Trash Companies
+# safer.fmcsa.dot.gov/CompanySnapshot.aspx 
+def trashComp1():
+    saferfmcsaPath = filedialog.askdirectory()
+    saferfmcsaFile = saferfmcsaPath+'/'+'saferfmcsa.csv'
+    Keyword = ['sanitation', 'trash', 'recycling', 'refuse', 'disposal', 'garbage', 'collection']
+    Excells = ['Business Name']
+    for x in Keyword:
+            trCount1 = 0
+            time.sleep(2)
+            headers = requests.utils.default_headers()
+            headers.update({
+                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+            })
+            print("https://safer.fmcsa.dot.gov/keywordx.asp?searchstring=%2A"+x+"%2A&SEARCHTYPE=")
+            page = requests.get("https://safer.fmcsa.dot.gov/keywordx.asp?searchstring=%2A"+x+"%2A&SEARCHTYPE=", headers=headers)
+            soup = BeautifulSoup(page.content,"lxml")
+            table = soup.find_all('table')[2]
+            for tr in table.find_all('tr'):
+                if(trCount1 > 0):
+                    for th in tr.find_all('th'):
+                        bussinessName = ""
+                        entityType = ""
+                        operationStatus = ""
+                        oosd = ""
+                        legalName = ""
+                        dbaName = ""
+                        physicalAddress = ""
+                        phone = ""
+                        mailingAddress = ""
+                        USDOTNumber = ""
+                        scidNumber = ""
+                        mcmxffNumber = ""
+                        DUNSNumber = ""
+                        powerUnit = ""
+                        driver = "" 
+                        formdate = ""
+                        mileage = ""
+                        oc = ""
+                        co = ""
+                        cc = ""
+                        b = th.find_all('b')[0]
+                        companyLink = b.findAll("a")[0]['href']
+                        companyPage = requests.get("https://safer.fmcsa.dot.gov/query.asp?searchtype=ANY&"+companyLink, headers=headers)
+                        companySoup = BeautifulSoup(companyPage.content,"lxml")
+                        font = companySoup.find_all('font')[1]
+                        bussinessName = font.find_all('b')[0].text
+                        detailTable = companySoup.find_all('table')[6]
+                        # entity
+                        entityType = detailTable.find_all('tr')[1].find_all('td')[0].text.replace(" ", "")
+                        # operation status & Out of Service Date
+                        operationStatus = detailTable.find_all('tr')[2].find_all('td')[0].text.replace(" ", "")
+                        oosd = detailTable.find_all('tr')[2].find_all('td')[1].text.replace(" ", "")
+                        # Legal Name
+                        legalName = detailTable.find_all('tr')[3].find_all('td')[0].text
+                        # DBA Name
+                        dbaName = detailTable.find_all('tr')[4].find_all('td')[0].text
+                        # Physical Address
+                        physicalAddress = detailTable.find_all('tr')[5].find_all('td')[0].text
+                        # Phone
+                        phone = detailTable.find_all('tr')[6].find_all('td')[0].text
+                        # Mailing Address
+                        mailingAddress = detailTable.find_all('tr')[7].find_all('td')[0].text
+                        # USDOT Number & State Carrier ID Number
+                        USDOTNumber = detailTable.find_all('tr')[8].find_all('td')[0].text
+                        scidNumber = detailTable.find_all('tr')[8].find_all('td')[1].text
+                        # MC/MX/FF Number(s) & DUNS Number
+                        mcmxffNumber = detailTable.find_all('tr')[9].find_all('td')[0].text
+                        DUNSNumber = detailTable.find_all('tr')[9].find_all('td')[1].text
+                        # Power Units & Drivers
+                        powerUnit = detailTable.find_all('tr')[10].find_all('td')[0].text
+                        driver = detailTable.find_all('tr')[10].find_all('td')[1].text
+                        # MCS-150 Form Date & MCS-150 Mileage (Year)
+                        formdate = detailTable.find_all('tr')[11].find_all('td')[0].text
+                        mileage = detailTable.find_all('tr')[11].find_all('td')[1].text
+                        # Operation Classification:
+                        OCTable =  detailTable.find_all('tr')[13].find_all('td')[0].find_all('table')[0]
+                        OCTr = OCTable.find_all('tr')[1]
+                        for td in OCTr.find_all('td'):
+                            if td.find("table"):
+                                TDTable = td.find_all('table')[0]
+                                TRTableCount = 0
+                                for tr in TDTable.find_all('tr'):
+                                    if(TRTableCount > 0):
+                                        if(tr.find_all('td')[0].text == "X"):
+                                            oc += tr.find_all('td')[1].find_all("font")[0].text + " - "
+                                    TRTableCount += 1
+                        # Carrier Operation:
+                        COTable =  detailTable.find_all('tr')[32].find_all('td')[0].find_all('table')[0]
+                        COTr = COTable.find_all('tr')[1]
+                        for td in COTr.find_all('td'):
+                            if td.find("table"):
+                                TDTable = td.find_all('table')[0]
+                                TRTableCount = 0
+                                for tr in TDTable.find_all('tr'):
+                                    if(TRTableCount > 0):
+                                        if(tr.find_all('td')[0].text == "X"):
+                                            co += tr.find_all('td')[1].find_all("font")[0].text + " - "
+                                    TRTableCount += 1
+                        # Cargo Carried:
+                        # print(detailTable.find_all('tr')[43])
+                        CCTable =  detailTable.find_all('tr')[43].find_all('td')[0].find_all('table')[0]
+                        CCTr = CCTable.find_all('tr')[1]
+                        for td in CCTr.find_all('td'):
+                            if td.find("table"):
+                                TDTable = td.find_all('table')[0]
+                                TRTableCount = 0
+                                for tr in TDTable.find_all('tr'):
+                                    if(TRTableCount > 0):
+                                        #print(tr)
+                                        #print(">>>>>>>>>>")
+                                        if(tr.find_all('td')[0].text == "X"):
+                                            if tr.find_all('td')[1].find("font"):
+                                                cc += tr.find_all('td')[1].find_all("font")[0].text + " - "
+                                            else:
+                                                cc += tr.find_all('td')[1].text + " - "
+                                    TRTableCount += 1
+                        print(cc)
+                        time.sleep(2)
+                trCount1 += 1
+            
+
+    finishLabel = tk.Label(root, text= 'DONE!', fg='green', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(200, 400, window=finishLabel)
 
 truePeopleSearchBtn = tk.Button(text='True People Search',command=selectFile, bg='brown',fg='white')
 AmbulanceBtn = tk.Button(text='Ambulance',command=scrapingNPI, bg='brown',fg='white')
 CrassociationBtn = tk.Button(text='Crassociation',command=loopThroughAlphabet, bg='brown',fg='white')
+trashBtn = tk.Button(text='Trash Companies',command=trashComp1, bg='brown',fg='white')
 
 canvas1.create_window(200, 50, window=truePeopleSearchBtn)
 canvas1.create_window(200, 150, window=AmbulanceBtn)
 canvas1.create_window(200, 250, window=CrassociationBtn)
+canvas1.create_window(200, 350, window=trashBtn)
 
 root.mainloop()
